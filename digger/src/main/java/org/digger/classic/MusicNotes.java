4b61ae34d83622570af054c46af0bc6e31815d6b
xafero <gberendt@outlook.com>
2021-06-05 10:33:21 +0200

    Playing music notes

---
+++ digger/src/main/java/org/digger/classic/MusicNotes.java
@@ -0,0 +1,146 @@
+package org.digger.classic;
+
+public class MusicNotes {
+
+	public final static byte NS = 1;
+
+	public final static byte C4 = ToneControl.C4;
+
+	public final static byte CM1 = C4 - 60;
+	public final static byte CSM1 = CM1 + 1;
+	public final static byte DM1 = CM1 + 2;
+	public final static byte DSM1 = CM1 + 3;
+	public final static byte EM1 = CM1 + 4;
+	public final static byte FM1 = CM1 + 5;
+	public final static byte FSM1 = CM1 + 6;
+	public final static byte GM1 = CM1 + 7;
+	public final static byte GSM1 = CM1 + 8;
+	public final static byte AM1 = CM1 + 9;
+	public final static byte ASM1 = CM1 + 10;
+	public final static byte BM1 = CM1 + 11;
+
+	public final static byte C0 = C4 - 48;
+	public final static byte CS0 = C0 + 1;
+	public final static byte D0 = C0 + 2;
+	public final static byte DS0 = C0 + 3;
+	public final static byte E0 = C0 + 4;
+	public final static byte F0 = C0 + 5;
+	public final static byte FS0 = C0 + 6;
+	public final static byte G0 = C0 + 7;
+	public final static byte GS0 = C0 + 8;
+	public final static byte A0 = C0 + 9;
+	public final static byte AS0 = C0 + 10;
+	public final static byte B0 = C0 + 11;
+
+	public final static byte C1 = C4 - 36;
+	public final static byte CS1 = C1 + 1;
+	public final static byte D1 = C1 + 2;
+	public final static byte DS1 = C1 + 3;
+	public final static byte E1 = C1 + 4;
+	public final static byte F1 = C1 + 5;
+	public final static byte FS1 = C1 + 6;
+	public final static byte G1 = C1 + 7;
+	public final static byte GS1 = C1 + 8;
+	public final static byte A1 = C1 + 9;
+	public final static byte AS1 = C1 + 10;
+	public final static byte B1 = C1 + 11;
+
+	public final static byte C2 = C4 - 24;
+	public final static byte CS2 = C2 + 1;
+	public final static byte D2 = C2 + 2;
+	public final static byte DS2 = C2 + 3;
+	public final static byte E2 = C2 + 4;
+	public final static byte F2 = C2 + 5;
+	public final static byte FS2 = C2 + 6;
+	public final static byte G2 = C2 + 7;
+	public final static byte GS2 = C2 + 8;
+	public final static byte A2 = C2 + 9;
+	public final static byte AS2 = C2 + 10;
+	public final static byte B2 = C2 + 11;
+
+	public final static byte C3 = C4 - 12;
+	public final static byte CS3 = C3 + 1;
+	public final static byte D3 = C3 + 2;
+	public final static byte DS3 = C3 + 3;
+	public final static byte E3 = C3 + 4;
+	public final static byte F3 = C3 + 5;
+	public final static byte FS3 = C3 + 6;
+	public final static byte G3 = C3 + 7;
+	public final static byte GS3 = C3 + 8;
+	public final static byte A3 = C3 + 9;
+	public final static byte AS3 = C3 + 10;
+	public final static byte B3 = C3 + 11;
+
+	public final static byte CS4 = C4 + 1;
+	public final static byte D4 = C4 + 2;
+	public final static byte DS4 = C4 + 3;
+	public final static byte E4 = C4 + 4;
+	public final static byte F4 = C4 + 5;
+	public final static byte FS4 = C4 + 6;
+	public final static byte G4 = C4 + 7;
+	public final static byte GS4 = C4 + 8;
+	public final static byte A4 = C4 + 9;
+	public final static byte AS4 = C4 + 10;
+	public final static byte B4 = C4 + 11;
+
+	public final static byte C5 = C4 + 12;
+	public final static byte CS5 = C5 + 1;
+	public final static byte D5 = C5 + 2;
+	public final static byte DS5 = C5 + 3;
+	public final static byte E5 = C5 + 4;
+	public final static byte F5 = C5 + 5;
+	public final static byte FS5 = C5 + 6;
+	public final static byte G5 = C5 + 7;
+	public final static byte GS5 = C5 + 8;
+	public final static byte A5 = C5 + 9;
+	public final static byte AS5 = C5 + 10;
+	public final static byte B5 = C5 + 11;
+
+	public final static byte C6 = C5 + 12;
+	public final static byte CS6 = C6 + 1;
+	public final static byte D6 = C6 + 2;
+	public final static byte DS6 = C6 + 3;
+	public final static byte E6 = C6 + 4;
+	public final static byte F6 = C6 + 5;
+	public final static byte FS6 = C6 + 6;
+	public final static byte G6 = C6 + 7;
+	public final static byte GS6 = C6 + 8;
+	public final static byte A6 = C6 + 9;
+	public final static byte AS6 = C6 + 10;
+	public final static byte B6 = C6 + 11;
+
+	public final static byte C7 = C6 + 12;
+	public final static byte CS7 = C7 + 1;
+	public final static byte D7 = C7 + 2;
+	public final static byte DS7 = C7 + 3;
+	public final static byte E7 = C7 + 4;
+	public final static byte F7 = C7 + 5;
+	public final static byte FS7 = C7 + 6;
+	public final static byte G7 = C7 + 7;
+	public final static byte GS7 = C7 + 8;
+	public final static byte A7 = C7 + 9;
+	public final static byte AS7 = C7 + 10;
+	public final static byte B7 = C7 + 11;
+
+	public final static byte C8 = C7 + 12;
+	public final static byte CS8 = C8 + 1;
+	public final static byte D8 = C8 + 2;
+	public final static byte DS8 = C8 + 3;
+	public final static byte E8 = C8 + 4;
+	public final static byte F8 = C8 + 5;
+	public final static byte FS8 = C8 + 6;
+	public final static byte G8 = C8 + 7;
+	public final static byte GS8 = C8 + 8;
+	public final static byte A8 = C8 + 9;
+	public final static byte AS8 = C8 + 10;
+	public final static byte B8 = C8 + 11;
+
+	public final static byte C9 = C8 + 12;
+	public final static byte CS9 = C9 + 1;
+	public final static byte D9 = C9 + 2;
+	public final static byte DS9 = C9 + 3;
+	public final static byte E9 = C9 + 4;
+	public final static byte F9 = C9 + 5;
+	public final static byte FS9 = C9 + 6;
+	public final static byte G9 = C9 + 7;
+}
